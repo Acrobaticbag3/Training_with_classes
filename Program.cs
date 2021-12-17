1b42bb4b144b37d01a06b10846817ec641d445a6
Acrobaticbag3 <kevintom214@gmail.com>
2021-12-17 14:09:58 +0100

    CLASSES!

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Training_with_classes {
+    class Program {
+        static void Main(string[] args) {
+            Car c = new Car();
+            c.setManufacturer();
+            c.setModle();
+            c.setModleYear();
+            c.setWeight();
+        }
+    }
+}
