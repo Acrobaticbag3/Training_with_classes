ccc2c4530d93faff40a3d9bc5dfa4d5f2f65d0e3
EmilWallett <89245357+Acrobaticbag3@users.noreply.github.com>
2022-08-25 12:27:33 +0200

    Cool stuff

--- Program.cs
+++ Program.cs
@@ -1,13 +1,34 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System;
 
 namespace Training_with_classes {
     class Program {
         static void Main(string[] args) {
             Car c = new Car();
-            c.setManufacturer();
-            c.setModle();
-            c.setModleYear();
-            c.setWeight();
+            c.setManufacturer("SomeRandomCarManu");
+            c.setModle("RandomModle");
+            c.setModleYear("RandomModleYear");
+            c.setWeight("RandomWeight");
+            
+            List<Car> carSpecs = new List<Car>();
+
+            carSpecs.Add(c);
         }
+    }
+
+    class Car {
+        private string manufacturer;
+        private string model;
+        private string modelYear; 
+        private string weight;
+
+        public void setManufacturer(string ma) { manufacturer = ma; }
+        public string setManufacturer() { return manufacturer; }
+        public void setModle(string mo) { model = mo; }
+        public string getModle() {return model;}
+        public void setModleYear(string m) { modelYear = m;}
+        public string setModleYear() { return modelYear; }
+        public void setWeight(string w) { weight = w;}
+        public string setWeight() { return weight; }
     }
 }
